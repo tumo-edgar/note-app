26963e28d7567ec4f399ef967c8ac5ffedf92d64
tumo-edgar <edgar.nikoghosyan1@tumo.org>
2025-07-09 19:56:05 +0400

    Init app

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-for="pix in arr">
+     <PixCounter v-if="pix.age <= 10" :name="pix.name" :age="pix.age" :count="pix.count" />
+  </div>
+ 
+
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+import PixCounter from './components/PixCounter.vue'
+import { ref } from 'vue';
+
+const arr = ref([
+  {
+    name: 'Luna',
+    age: 5,
+    count: 2
+  },
+  {
+    name: 'John',
+    age: 12,
+    count: 34
+  },
+  {
+    name: 'Aaaa',
+    age: 3,
+    count: 5
+  },
+
+])
+
+</script>
+
+<style scoped></style>
---
+++ src/components/PixCounter.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="card">
+        <h1>{{ props.name }}</h1>
+        <h2>{{ props.age }}</h2>
+        <h1>{{ counter }}</h1>
+
+        <button @click="pixShatacnel">Click me</button>
+    </div>
+</template>
+
+<script setup>
+import { ref, defineProps } from 'vue'
+
+const props = defineProps(['name', 'age', 'count'])
+
+let counter = ref(props.count)
+
+function pixShatacnel() {
+    counter.value++
+}
+</script>
+
+<style>
+    .card {
+        width: 300px;
+        height: 300px;
+        border: 1px solid gray;
+        border-radius: 6px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+    }
+</style>

a3d53d0759f3839d3e7f666fd55e4169b0859019
tumo-edgar <edgar.nikoghosyan1@tumo.org>
2025-07-09 21:23:08 +0400

    Added Card

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,41 @@
 <template>
-  <div v-for="pix in arr">
-     <PixCounter v-if="pix.age <= 10" :name="pix.name" :age="pix.age" :count="pix.count" />
-  </div>
- 
+  <main class="main">
+    <h1>Note App</h1>
 
-  <RouterView />
+    <div class="notes">
+      <Card />
+    </div>
+
+  </main>
 </template>
 
 <script setup>
-import { RouterView } from 'vue-router'
-import PixCounter from './components/PixCounter.vue'
-import { ref } from 'vue';
-
-const arr = ref([
-  {
-    name: 'Luna',
-    age: 5,
-    count: 2
-  },
-  {
-    name: 'John',
-    age: 12,
-    count: 34
-  },
-  {
-    name: 'Aaaa',
-    age: 3,
-    count: 5
-  },
-
-])
+import Card from './components/Card.vue'
 
 </script>
 
-<style scoped></style>
+<style>
+body,
+#app {
+  width: 100%;
+  height: 100%;
+  color: #000;
+}
+
+main.main {
+  background: linear-gradient(128deg, rgba(135, 246, 180, 0.84) 12.75%, rgba(50, 180, 187, 0.67) 83.4%);
+  width: 100%;
+  min-height: 100vh;
+}
+
+h1 {
+  font-family: Inter;
+  font-weight: 700;
+  font-style: Bold;
+  font-size: 32px;
+  line-height: 100%;
+  letter-spacing: 0%;
+  text-align: center;
+  padding: 24px;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="card">
+        <p class="tile">My Title</p>
+        <p class="content">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio et, temporibus obcaecati est quas vero iste, dolorum voluptate libero praesentium fugit, debitis delectus itaque optio. Dolorum, aut veniam! Id, obcaecati.
+        </p>
+        <p class="time">05-07-2025 20:30</p>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    .card {
+        width: 300px;
+        height: 210px;
+        border: 1px solid #fff;
+        border-radius: 14px;
+        background: linear-gradient(114.43deg, rgba(255, 255, 255, 0.6) 4%, rgba(255, 255, 255, 0.1) 100%);
+    }
+
+</style>
